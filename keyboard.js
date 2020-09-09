85f42fc150a43434a0d344e9cec39b01d0dc0154
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-09-08 19:46:30 -0500

    Create the main keyboard elements to be added to the page

--- keyboard.js
+++ keyboard.js
@@ -18,6 +18,10 @@ const Keyboard = {
   init() {
     const { elements: { main, keysContainer, keys }, _createKeys } = this;
 
+    // Create the main elements
+    main = document.createElement('div');
+    keysContainer = document.createElement('div');
+
   },
 
 }
